f3a46dbc54d0d2c6d183437e178f2c778b10b9bd
Fuzail Ahemd <fuzailcode@gmail.com>
2025-05-03 15:53:25 +0500

    May 3 25

---
+++ Java-Script/7.Array Methods/script.js
@@ -0,0 +1,42 @@
+// var names = ['fuzail' , 'selat' , 'ahmed' , 'raza', 'fuzail', 'usman'];
+// console.log(names);
+//! find index number [ .indexof ]
+// var findIndexNum = names.indexOf('raza');
+// console.log(findIndexNum);
+//? noted point 
+// 1.Give Array value to get indexnumber 
+// 2.More than 1 time value he will return first one 
+// 3.Ager value array ma nahi ha tu output -1 huga
+
+//! last index of
+// var names = ['fuzail' , 'selat' , 'ahmed' , 'raza', 'fuzail', 'usman','fuzail'];
+// console.log(names);
+// var findIndexNum = names.lastIndexOf('fuzail');
+// console.log(findIndexNum);
+
+var namesList1 = ["John", "Smith", "Andy"];
+var namesList2 = ["Trish", "Rock" , "Neil"];
+I
+var yourName = prompt("Enter your name");
+var friendName = prompt("Enter your friend name");
+var anotherFriendName = prompt("Enter your another friend name");
+/*
+You are not erxist in the system
+*/
+
+if (yourName ||"John", "Smith", "Andy") {
+    if(friendName || "Trish", "Rock" , "Neil"){
+        if (anotherFriendName || "Trish", "Rock" , "Neil") {
+            console.log(anotherFriendName);
+            
+        }
+        else{
+            console.log('You are not erxist in the system');
+        }
+    }
+    else{
+        console.log('You are not erxist in the system');
+    }
+} else{
+    console.log('You are not erxist in the system');
+}
