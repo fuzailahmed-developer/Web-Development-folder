6327660f673b2ca627d91104bf845a81539ae09a
Fuzail Ahemd <fuzailcode@gmail.com>
2025-05-06 17:48:45 +0500

    add more

---
+++ Java-Script/8. For Loop/script.js
@@ -0,0 +1,24 @@
+//! for loop 
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//! for loop with array 
+// var games = ['cricket' , 'hockey' , 'football' , 'baseball'];
+// // console.log(games.length);
+// for (var i = 0; i < games.length ; i++) {
+// console.log(games[i]);
+// }
+
+// var refGen = ['fridge', 'gen 1' , 'ho power 15' , 'mar x 20'];
+// for(var i = 0; i < refGen ; i++){
+//     console.log(refGen[i]);
+//    document.write(refGen[i] + '<br>')
+// }
+
+
+let fruits = ["apple", "banana", "mango"];
+
+  for (let i = 0; i < fruits.length; i++) {
+    document.write(fruits[i] + "<br>");
+  }
