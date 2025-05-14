54f89a64c0017d9bd5e3ff56c74730672abb9fe6
Fuzail Ahemd <fuzailcode@gmail.com>
2025-05-14 12:26:00 +0500

    for loop updated

--- Java-Script/8. For Loop/script.js
+++ Java-Script/8. For Loop/script.js
@@ -1,24 +1,38 @@
 //! for loop 
-// for (var i = 1; i <= 10; i++) {
-//     console.log(i);
+//? prnit number 1 to 10 
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//? sum number 1 to 10
+// var sum = 0;
+// for(var i = 0; i <= 10; i++){
+//   sum = sum + i;
 // }
+// console.log(sum);
 
 //! for loop with array 
 // var games = ['cricket' , 'hockey' , 'football' , 'baseball'];
-// // console.log(games.length);
-// for (var i = 0; i < games.length ; i++) {
-// console.log(games[i]);
+
+// for(var i = 0; i < games.length; i++){
+
+//   console.log(games[i]);
+  
 // }
 
-// var refGen = ['fridge', 'gen 1' , 'ho power 15' , 'mar x 20'];
-// for(var i = 0; i < refGen ; i++){
-//     console.log(refGen[i]);
-//    document.write(refGen[i] + '<br>')
-// }
-
-
-let fruits = ["apple", "banana", "mango"];
-
-  for (let i = 0; i < fruits.length; i++) {
-    document.write(fruits[i] + "<br>");
-  }
+//! for loop with array and print name in output Screen
+let muslimNames = [
+  "Aisha",
+  "Fatima",
+  "Khadija",
+  "Hassan",
+  "Hussain",
+  "Omar",
+  "Ali",
+  "Zainab",
+  "Abdullah",
+  "Maryam"
+];
+for(var i = 0; i < muslimNames.length; i++){
+  console.log(muslimNames[i]);
+  document.write(muslimNames[i] + "<br>")
+}
