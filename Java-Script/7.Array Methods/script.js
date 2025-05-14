c80987b7fa6e80ac29494ab7b2eb49ceb52a709b
Fuzail Ahemd <fuzailcode@gmail.com>
2025-05-14 11:39:48 +0500

    Updated

--- Java-Script/7.Array Methods/script.js
+++ Java-Script/7.Array Methods/script.js
@@ -1,42 +1,54 @@
-// var names = ['fuzail' , 'selat' , 'ahmed' , 'raza', 'fuzail', 'usman'];
-// console.log(names);
+var muslimNames = [
+    "Aisha",
+    "Fatima",
+    "Khadija",
+    "Hassan",
+    "Hussain",
+    "Omar",
+    "Ali",
+    "Zainab",
+    "Abdullah",
+    "Maryam",
+    "Abdullah"
+];
+
 //! find index number [ .indexof ]
-// var findIndexNum = names.indexOf('raza');
-// console.log(findIndexNum);
+// var findTarget = muslimNames.indexOf("Hussain");
+// console.log(findTarget);
 //? noted point 
 // 1.Give Array value to get indexnumber 
 // 2.More than 1 time value he will return first one 
 // 3.Ager value array ma nahi ha tu output -1 huga
 
-//! last index of
-// var names = ['fuzail' , 'selat' , 'ahmed' , 'raza', 'fuzail', 'usman','fuzail'];
-// console.log(names);
-// var findIndexNum = names.lastIndexOf('fuzail');
-// console.log(findIndexNum);
+//! task 
+var yourNameList = ["Amir","Fatima","Khadija"];
+var friendNameList1 = ["Hassan","Hussain","Omar"];
+var anotherFriendList2 = ["Ali","Zainab","Abdullah"];
 
-var namesList1 = ["John", "Smith", "Andy"];
-var namesList2 = ["Trish", "Rock" , "Neil"];
-I
-var yourName = prompt("Enter your name");
-var friendName = prompt("Enter your friend name");
-var anotherFriendName = prompt("Enter your another friend name");
-/*
-You are not erxist in the system
-*/
+var yourName = prompt("Enter Your Name");
 
-if (yourName ||"John", "Smith", "Andy") {
-    if(friendName || "Trish", "Rock" , "Neil"){
-        if (anotherFriendName || "Trish", "Rock" , "Neil") {
-            console.log(anotherFriendName);
-            
+if (yourName === "Amir" || yourName === "Fatima" || yourName === "Khadija" ) {
+
+    var friendName = prompt("Enter Your Friend Name") ;
+    
+    if (friendName === "Hassan" || friendName === "Hussain" || friendName === "Omar") {
+        
+        var anotherFriend = prompt("Enter another Friend Name");
+
+        if (anotherFriend === "Ali" || anotherFriend === "Zainab" || anotherFriend === "Abdullah") {
+            console.log(
+            'My name is : ' + yourName +
+            '\nMy friend name is : ' + friendName +
+            '\nMy another friend name is : ' + anotherFriend
+            );
+        } else{
+            console.log("another Friend Name Not Match");
         }
-        else{
-            console.log('You are not erxist in the system');
-        }
-    }
+    } 
     else{
-        console.log('You are not erxist in the system');
+        console.log("You Friend Name is Not Match");
     }
-} else{
-    console.log('You are not erxist in the system');
+} 
+else {
+    console.log("You Name Not Match");
 }
