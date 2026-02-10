7d59866fd5b493206fa4c7606c8165337fb83469
Fuzail Ahemd <fuzailcode@gmail.com>
2026-02-10 15:18:24 +0500

    updated

---
+++ TypeScript/01_Basic/index.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    return "Hello, ".concat(name);
+}
+console.log(greet('ali'));
+var userName = 'ali';
+console.log(userName);
---
+++ TypeScript/01_Basic/index.ts
@@ -0,0 +1,7 @@
+function greet(name : string):string{
+    return `Hello, ${name}`
+}
+console.log(greet('ali'))
+
+let userName : string = 'ali'
+console.log(userName)
